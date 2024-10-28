b983c54ac6b79b37317d2f3fd91397781161351a
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2024-10-28 12:31:40 +0530

    landing page button added

--- src/components/landingpage/index.jsx
+++ src/components/landingpage/index.jsx
@@ -11,8 +11,8 @@ const LandingPage = () => {
     return <Layout>
         <img src={bgImage} width="100%" height="100%" alt="landing-page-image" />
         <Box className="landing-page-info">
-            <Box component="h4">Welcome to eGwala</Box>
-            <Button color='primary'>Lets start</Button>
+            <Box component="h2" fontWeight='400'>Welcome to eGwala</Box>
+            <Button color='primary' variant='outlined'>Lets start</Button>
         </Box>
     </Layout>
 }
