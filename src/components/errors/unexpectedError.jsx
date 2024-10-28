3ad8d38eab03a5f72b2040e7ad94d0cbbc3c43f6
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2024-10-28 13:33:06 +0530

    landing page done

--- src/components/errors/unexpectedError.jsx
+++ src/components/errors/unexpectedError.jsx
@@ -1,16 +1,22 @@
-import { useRouteError } from "react-router-dom";
+import { Button } from "@mui/material";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function UnexpectedError() {
   const error = useRouteError();
   console.error(error);
 
+  const navigate = useNavigate();
+
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <Button variant="text" onClick={() => navigate(-1)}>Go Back</Button>
+      <div id="error-page" className="d-f fd-c">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </>
   );
 }
--- src/components/landingpage/index.jsx
+++ src/components/landingpage/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import Layout from '../Layout';
 import bgImage from './../../assets/images/background.png'
 import { Box, Button } from '@mui/material';
@@ -7,12 +7,18 @@ import { Box, Button } from '@mui/material';
 const LandingPage = () => {
     const { contacts } = useLoaderData();
     console.log(contacts);
+    const navigate = useNavigate();
 
     return <Layout>
         <img src={bgImage} width="100%" height="100%" alt="landing-page-image" />
         <Box className="landing-page-info">
             <Box component="h2" fontWeight='400'>Welcome to eGwala</Box>
-            <Button color='primary' variant='outlined'>Lets start</Button>
+            <Button sx={{
+                backgroundImage:'linear-gradient(to top, #ff66b3, #1976d2)',
+                color: '#fff',
+            }}
+            onClick={() => navigate('/login')}
+            >Lets start</Button>
         </Box>
     </Layout>
 }
